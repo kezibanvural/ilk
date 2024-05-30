efa9665c27d7d86b723e474d5bf1358246de1b82
enskerti <enskerti@gmail.com>
2024-05-30 17:10:02 +0300

    sign up page added

--- src/components/main/common/navbar/index.jsx
+++ src/components/main/common/navbar/index.jsx
@@ -5,47 +5,48 @@ import React from "react";
 const MainNavbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-transparent px-3">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarText"
-          aria-controls="navbarText"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarText">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" href="/">
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="/services">
-                Services
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="/app-details">
-                App Details
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="/contact">
-                Contact
-              </Link>
-            </li>
-          </ul>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-transparent px-3 z-3">
+        <div class="container-fluid">
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <Link class="nav-link active" aria-current="page" href="/">
+                  Home
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" href="/services">
+                  Services
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" href="/app-details">
+                  App Details
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link" href="/contact">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <Link class="navbar-brand" href="/">
+            <Image src="/logos/Size=Full.svg" width={71} height={46} alt="logo"/>
+          </Link>
         </div>
-        <Link className="navbar-brand" href="/">
-          <Image src="/logos/Size=Full.svg" width={71} height={46} alt="logo"/>
-        </Link>
       </nav>
-      
     </>
   );
 };
