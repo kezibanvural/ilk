91dfc1263d6c0c054616353e139e4a951dfa003e
enskerti <enskerti@gmail.com>
2024-07-13 20:20:47 +0300

    next.config changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      AUTH_SECRET: process.env.AUTH_SECRET,
+      NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+      AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+      API_URL: process.env.API_URL,
+    },
+  };
+  
+  export default nextConfig;
+  
