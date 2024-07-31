0d4480d5db9327e38c1875176edcc739fe472642
enskerti <enskerti@gmail.com>
2024-07-31 12:26:22 +0300

    next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+        AUTH_TRUSTED_HOSTS: process.env.AUTH_TRUSTED_HOSTS,
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+    experimental: {
+      trustHostHeader: true,
+    },
+  };
+  
+  export default nextConfig;
